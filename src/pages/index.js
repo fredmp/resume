91588f8883cb720fcbd6ae82483ed29824b8a278
Frederico Martins Porto <martinsporto@gmail.com>
2019-02-28 18:45:50 -0300

    Fix index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,37 @@
+import './index.scss';
 import React from 'react';
-import { Link } from 'gatsby';
 
 import Layout from '../components/layout';
-import Image from '../components/image';
 import SEO from '../components/seo';
+import emailIcon from '../images/icons/email.svg';
+import calendarIcon from '../images/icons/calendar.svg';
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+    <SEO title="Home" keywords={['portfolio', 'resume', 'ruby', 'javascript', 'react']} />
+    <div className="main-text">
+      <div className="text-content has-text-centered">
+        <p>
+          Hi! My name is Frederico. I&apos;m a software engineer living in Rio de Janeiro, Brazil.
+        </p>
+        <p>
+          Currently, I work primarily with Ruby (Rails) and Javascript (React, Node, Express and
+          Vue).
+        </p>
+        <p>I&apos;m available for remote projects! Please feel free to contact me.</p>
+      </div>
+      <h4>Get in touch</h4>
+      <div className="contact-links">
+        <a href="/#" className="navbar-item social button is-primary">
+          <img src={emailIcon} alt="Contact" width="40" height="30" />
+          <span>Contact me</span>
+        </a>
+        <a href="/#" className="navbar-item social button is-primary">
+          <img src={calendarIcon} alt="Schedule" width="40" height="30" />
+          <span>Schedule a call</span>
+        </a>
+      </div>
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 );
 
